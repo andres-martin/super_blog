325f29b9139ea6ad79a6411feb57dcc1ef94eb17
andres-martin <andresmartin266@gmail.com>
2019-08-01 18:45:40 -0500

    [feature] create user form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/index'
   # get 'posts/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#index'
   resources :posts
+
+  resources :users, only: [:new, :create]
 end
