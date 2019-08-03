777ce882da8d1eecd72d2285021d0c3517bcca47
andres-martin <andresmartin266@gmail.com>
2019-08-02 23:57:33 -0500

    sign up form password confirmation in progress

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,13 +11,16 @@
 #
 
 class User < ApplicationRecord
+    before_save {self.email = email.downcase}
     has_secure_password validations: false
     
     has_many :posts
     has_many :comments
 
-    validates :name, presence: true
-    validates :email, uniqueness: true, format: /@/
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+    validates :name, presence: true, length: {maximum: 50}
+    validates :email, presence: true, uniqueness: { case_sensitive: false }, format: {with: VALID_EMAIL_REGEX}
     validates :password, presence: true, on: :create
-    validates :password, length: {minimum:6, maximum:20}, allow_nil: true
+    validates :password, length: {minimum:8, maximum:20}, allow_nil: true
 end
